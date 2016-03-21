4640302e9dfef4f509ef47b86cb728f6c836efb8
zixuan-zhang <zixuan.zhang.victor@gmail.com>
2016-03-21 21:10:57 +0800

    interleaving string

---
+++ 097_interleaving_string.cpp
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ *  @File  : 097_interleaving_string.cpp
+ *  @Author: Zhang Zixuan
+ *  @Email : zixuan.zhang.victor@gmail.com
+ *  @Blog  : www.noathinker.com
+ *  @Date  : 2016年03月21日 星期一 21时10分01秒
+ ******************************************************************************/
+
+/*
+ * Question:
+ *
+ * Given s1, s2, s3, find whether s3 is formed by the interleaving of s1 and s2.
+ *
+ * For example,
+ * Given:
+ * s1 = "aabcc",
+ * s2 = "dbbca",
+ *
+ * When s3 = "aadbbcbcac", return true.
+ * When s3 = "aadbbbaccc", return false.
+ *
+ */
+
+
+class Solution {
+public:
+
+    bool isInterleave(string s1, string s2, string s3) {
+        if (s1.length()+s2.length() != s3.length())
+            return false;
+        if (s1.length() == 0)
+            return s2 == s3;
+        if (s2.length() == 0)
+            return s1 == s3;
+
+        int len1 = s1.length();
+        int len2 = s2.length();
+
+        vector<vector<int> > matrix;
+        vector<int> vec(len2+1, 1);
+        for (int i = 0; i < len1+1; ++i)
+            matrix.push_back(vec);
+
+        // initialize
+        for (int i = 1; i <= len1; ++i)
+            matrix[i][0] = (matrix[i-1][0] && s1[i-1] == s3[i-1]);
+        for (int i = 1; i <= len2; ++i)
+            matrix[0][i] = (matrix[0][i-1] && s2[i-1] == s3[i-1]);
+
+        for (int i = 1; i <= len1; ++i)
+        {
+            for (int j = 1; j <= len2; ++j)
+            {
+                matrix[i][j] = (matrix[i-1][j] && s1[i-1] == s3[i+j-1]) || 
+                    (matrix[i][j-1] && s2[j-1] == s3[i+j-1]);
+            }
+        }
+        return matrix[len1][len2];
+    }
+};
