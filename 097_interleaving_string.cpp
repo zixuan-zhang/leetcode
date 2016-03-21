ba45b4f11f72b756b5000bc85e909b627b9521fa
zixuan-zhang <zixuan.zhang.victor@gmail.com>
2016-03-21 21:14:45 +0800

    interleaving string, add other method

--- 097_interleaving_string.cpp
+++ 097_interleaving_string.cpp
@@ -22,10 +22,92 @@
  */
 
 
-class Solution {
+class Solution
+{
 public:
 
-    bool isInterleave(string s1, string s2, string s3) {
+    bool isInter(string s1, int start1, string s2, int start2, string s3, int start3)
+    {
+        if (s3.length() == start3)
+            return true;
+        bool result = false;
+        if (start1 < s1.length() && s1[start1] == s3[start3])
+            result = isInter(s1, start1+1, s2, start2, s3, start3+1);
+        if (result)
+            return result;
+        if (start2 < s2.length() && s2[start2] == s3[start3])
+            result = isInter(s1, start1, s2, start2+1, s3, start3+1);
+        return result;
+    }
+
+    bool isInterleave(string s1, string s2, string s3)
+    {
+        if (s1.length() + s2.length() != s3.length())
+            return false;
+        int start1 = 0;
+        int start2 = 0;
+        int start3 = 0;
+        return isInter(s1, start1, s2, start2, s3, start3);
+    }
+
+    bool isInterleaveWithStack(string s1, string s2, string s3)
+    {
+        if (s1.length()+s2.length() != s3.length())
+            return false;
+
+        vector<int> vec(s3.length());
+
+        stack<int> stack1;
+        stack<int> stack2;
+        stack<int> stack3;
+        bool res = false;
+        if (s1.length() > 0 && s1[0] == s3[0])
+        {
+            stack1.push(1);
+            stack2.push(0);
+            stack3.push(1);
+            res = true;
+        }
+
+        if (s2.length() >0 && s2[0] == s3[0])
+        {
+            stack1.push(0);
+            stack2.push(1);
+            stack3.push(1);
+            res = true;
+        }
+        if (!res)
+            return false;
+
+        int index1, index2, index3;
+        while (!stack3.empty())
+        {
+            index1 = stack1.top();
+            index2 = stack2.top();
+            index3 = stack3.top();
+            stack1.pop();
+            stack2.pop();
+            stack3.pop();
+            if (index3 == s3.length())
+                return true;
+            if (index1 < s1.length() && s1[index1] == s3[index3])
+            {
+                stack1.push(index1+1);
+                stack2.push(index2);
+                stack3.push(index3+1);
+            }
+            if (index2 < s2.length() && s2[index2] == s3[index3])
+            {
+                stack1.push(index1);
+                stack2.push(index2+1);
+                stack3.push(index3+1);
+            }
+        }
+        return false;
+    }
+
+    bool isInterleavingWithDP(string s1, string s2, string s3)
+    {
         if (s1.length()+s2.length() != s3.length())
             return false;
         if (s1.length() == 0)
@@ -51,10 +133,11 @@ public:
         {
             for (int j = 1; j <= len2; ++j)
             {
-                matrix[i][j] = (matrix[i-1][j] && s1[i-1] == s3[i+j-1]) || 
+                matrix[i][j] = (matrix[i-1][j] && s1[i-1] == s3[i+j-1]) ||
                     (matrix[i][j-1] && s2[j-1] == s3[i+j-1]);
             }
         }
         return matrix[len1][len2];
     }
 };
+
