b7dd9b3a00470ee7e048215264777dcba0914325
zixuan-zhang <zixuan.zhang.victor@gmail.com>
2016-07-28 21:06:49 +0800

    course schedule

---
+++ 207_course_schedule.cpp
@@ -0,0 +1,100 @@
+/*******************************************************************************
+ *  @File  : 207_course_schedule.cpp
+ *  @Author: Zhang Zixuan
+ *  @Email : zixuan.zhang.victor@gmail.com
+ *  @Blog  : www.noathinker.com
+ *  @Date  : Thu 28 Jul 2016 08:40:07 PM CST
+ ******************************************************************************/
+
+/*
+ * Question:
+ *
+ * There are a total of n courses you have to take, labeled from 0 to n - 1.
+ * Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+ * Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+ * For example:
+ * 2, [[1,0]]
+ * There are a total of 2 courses to take. To take course 1 you should have finished course 0. So it is possible.
+ * 2, [[1,0],[0,1]]
+ * There are a total of 2 courses to take. To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
+ *
+ */
+
+#include <iostream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+
+class Solution
+{
+public:
+    bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites)
+    {
+        vector<int> visited;
+        stack<int> courses;
+        unordered_map<int, unordered_set<int> > pres;
+        unordered_map<int, unordered_set<int> > follows;
+
+        // Initialize
+        for (int i = 0; i < numCourses; ++i)
+        {
+            unordered_set<int> requisites;
+            pres[i] = requisites;
+            unordered_set<int> follow;
+            follows[i] = follow;
+        }
+
+        for (int i = 0; i < prerequisites.size(); ++i)
+        {
+            pres[prerequisites[i].first].insert(prerequisites[i].second);
+            follows[prerequisites[i].second].insert(prerequisites[i].first);
+        }
+
+        // Find items with no requisites
+        for (unordered_map<int, unordered_set<int> >::iterator it = pres.begin(); it != pres.end(); ++it)
+        {
+            if (it->second.size() == 0)
+                courses.push(it->first);
+        }
+
+        while (!courses.empty())
+        {
+            int course = courses.top();
+            visited.push_back(course);
+            courses.pop();
+            
+            unordered_set<int> follow = follows[course];
+            for (auto item = follow.begin(); item != follow.end(); item++)
+            {
+                pres[*item].erase(course);
+                if (pres[*item].size() == 0)
+                    courses.push(*item);
+            }
+        }
+
+        if (visited.size() == numCourses)
+            return true;
+        else
+            return false;
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<pair<int, int> > prerequisites;
+    prerequisites.push_back(make_pair(3, 1));
+    prerequisites.push_back(make_pair(2, 1));
+    prerequisites.push_back(make_pair(2, 0));
+    prerequisites.push_back(make_pair(1, 0));
+    prerequisites.push_back(make_pair(1, 4));
+    prerequisites.push_back(make_pair(4, 3));
+    bool result = s.canFinish(4, prerequisites);
+    cout<<result<<endl;
+
+    return 0;
+}
